f2e17f14eb6f995ac494098f50b28a19cdd71174
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-16 15:04:22 -0300

    Fazendo a adequação da imagem
    Aqui pegamos o tamanho da tela do celular e fazemos o calculo, para que fique responsivo em todos os dispositivos

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet,Image } from 'react-native';
+import { StyleSheet,Image, Dimensions } from 'react-native';
 
 import topo from '../../../assets/topo.png'
+
+const width = Dimensions.get('screen').width;
 
 export default function Carrinho (){
     return <Image source={topo} style={estilos.topo}/>
@@ -9,7 +11,8 @@ export default function Carrinho (){
 const estilos = StyleSheet.create({
 
     topo:{
-        width: "100%"
+        width: "100%",
+        height: 578 / 768 * width
     }
 
 })
