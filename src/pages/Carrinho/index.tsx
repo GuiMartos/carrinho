2d2a7227ee8f2966ee3d83017274f8bb5c4c4bca
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-16 14:58:36 -0300

    Começando a estilização da img

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -4,8 +4,12 @@ import { StyleSheet,Image } from 'react-native';
 import topo from '../../../assets/topo.png'
 
 export default function Carrinho (){
-    return <Image source={topo}/>
+    return <Image source={topo} style={estilos.topo}/>
 }
 const estilos = StyleSheet.create({
+
+    topo:{
+        width: "100%"
+    }
 
 })
