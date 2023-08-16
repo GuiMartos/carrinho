a1a898b51665fca7f3adb67c72c5805ac7da92c8
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-16 15:07:16 -0300

    Update index.tsx

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -8,11 +8,10 @@ const width = Dimensions.get('screen').width;
 export default function Carrinho (){
     return <Image source={topo} style={estilos.topo}/>
 }
-const estilos = StyleSheet.create({
 
+const estilos = StyleSheet.create({
     topo:{
         width: "100%",
         height: 578 / 768 * width
     }
-
 })
