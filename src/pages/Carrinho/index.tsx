8d242e5d69b746ec6a3bb34f1238b011839c2412
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-31 21:41:59 -0300

    Estilização

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import { StyleSheet,Image, Dimensions, Text, View } from 'react-native';
+import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
+import topo from '../../../assets/topo.png';
+import logo from '../../../assets/logo.png';
 
-import topo from '../../../assets/topo.png'
 
 const width = Dimensions.get('screen').width;
 
-export default function Carrinho (){
+export default function Carrinho(){
     return <>
-    <Image source={topo} style={estilos.topo}/>
-    <Text style={estilos.titulo}>Detalhe do carrinho</Text>
-    <View>
-        <Text>Carrinho de compras</Text>
-        <Text>Fazenda Etec Cidade do Livro</Text>
-        <Text>Uma cesta com produtos selecionados cuidadosamente
-            da fazenda direto para sua cozinha
-        </Text>
-        <Text>R$ 40,00</Text>
-    </View>
-    </> 
-}
+    <Image source={topo} style={estilos.topo}></Image>
 
-const estilos = StyleSheet.create({
-    topo:{
+    <Text style={estilos.titulo}>Detalhe do carrinho</Text>
+
+    <View style={estilos.carrinho}>
+
+        <Text style={estilos.titulocarrinho}>Carrinho de compra</Text>
+        <View style={estilos.fazenda}>
+            <Image style={estilos.imgFazenda} source={logo}></Image>
+            <Text style={estilos.nomeFazenda}>Fazenda Etec Cidade do Livro</Text>
+        </View>
+        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente
+            da fazenda direto para sua cozinha 
+        </Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
+    </View>
+    </>
+}
+const estilos= StyleSheet.create({
+    topo: {
         width: "100%",
-        height: 578 / 768 * width
+        height: 578/768 * width
     },
-    titulo:{
-        fontSize: 24,
+    titulo: {
+        fontSize: 24, 
         fontWeight: "bold",
         lineHeight: 26,
         width: "100%",
@@ -34,6 +40,40 @@ const estilos = StyleSheet.create({
         color: "white",
         position: "absolute",
         padding: 16
+    },
+    titulocarrinho:{
+        fontSize: 26,
+        lineHeight: 46,
+        color: "#464646",
+        fontFamily: "MontserratBold"
+    },
+    carrinho:{
+        paddingVertical:8,
+        paddingHorizontal: 16
+    },
+    nomeFazenda:{
+        fontSize: 16,
+        lineHeight:26,
+        marginLeft: 12,
+        fontWeight: "bold"
+    },
+    imgFazenda:{
+        width: 32,
+        height: 32
+    },
+    fazenda:{
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+    descricao:{
+        color:"#a3a3a3",
+        fontStyle: "italic"
+    },
+    preco:{
+        fontSize: 26,
+        lineHeight:42,
+        color: "#2a9f85",
+        marginTop: 8
     }
 
 })
