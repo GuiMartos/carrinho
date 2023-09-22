ae48daedf83dc321cd5a4d2b969b215184ac6a0b
GuiMartos <guilhermemartos2005@gmail.com>
2023-09-22 11:31:41 -0300

    Dividindo o componente "Topo"

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
-import topo from '../../../assets/topo.png';
 import logo from '../../../assets/logo.png';
-
-
-const width = Dimensions.get('screen').width;
 
 export default function Carrinho(){
     return <>
-    <Image source={topo} style={estilos.topo}></Image>
-
-    <Text style={estilos.titulo}>Detalhe do carrinho</Text>
 
     <View style={estilos.carrinho}>
 
@@ -27,20 +20,6 @@ export default function Carrinho(){
     </>
 }
 const estilos= StyleSheet.create({
-    topo: {
-        width: "100%",
-        height: 578/768 * width
-    },
-    titulo: {
-        fontSize: 24, 
-        fontWeight: "bold",
-        lineHeight: 26,
-        width: "100%",
-        textAlign: "center",
-        color: "white",
-        position: "absolute",
-        padding: 16
-    },
     titulocarrinho:{
         fontSize: 26,
         lineHeight: 46,
